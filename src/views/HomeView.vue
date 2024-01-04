46179eb7a5fd8f765bada23cfbbe2ed5fbd0d4ff
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-01-04 09:56:22 +0500

    beginner

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p  class="text-3xl font-bold underline text-wrap text-slate-600">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, harum
+      aspernatur eos officiis dolorem nam facere! Consequatur, dolores illo
+      saepe laboriosam ad ipsam porro, molestias odio minus magni facere vitae?
+    </p>
+    <p class="text-3xl font-bold underline">Hello world!</p>
+    <p class="underline decoration-solid ...">The quick brown fox...</p>
+    <p class="underline decoration-double ...">The quick brown fox...</p>
+    <p class="underline decoration-dotted ...">The quick brown fox...</p>
+    <p class="underline decoration-dashed ...">The quick brown fox...</p>
+    <p class="underline decoration-wavy ...">The quick brown fox...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
