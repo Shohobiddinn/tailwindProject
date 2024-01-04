49ad13eacf7513d3083dfe046da2f2055e350a08
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-01-04 10:51:57 +0500

    tailwand update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
+
 <template>
-  <div>
-    <p  class="text-3xl font-bold underline text-wrap text-slate-600">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, harum
-      aspernatur eos officiis dolorem nam facere! Consequatur, dolores illo
-      saepe laboriosam ad ipsam porro, molestias odio minus magni facere vitae?
-    </p>
-    <p class="text-3xl font-bold underline">Hello world!</p>
-    <p class="underline decoration-solid ...">The quick brown fox...</p>
-    <p class="underline decoration-double ...">The quick brown fox...</p>
-    <p class="underline decoration-dotted ...">The quick brown fox...</p>
-    <p class="underline decoration-dashed ...">The quick brown fox...</p>
-    <p class="underline decoration-wavy ...">The quick brown fox...</p>
-  </div>
+  <p class="break-keep text-bold cursor-pointer">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis ducimus
+    fugiat veniam, et mollitia ad itaque tenetur culpa, hic distinctio quas quia
+    velit repellendus, consectetur fugit ea possimus voluptates nemo!
+  </p>
 </template>
+
 
 <script>
 export default {
